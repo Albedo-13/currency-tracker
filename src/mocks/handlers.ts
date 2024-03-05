fa79528d4bb2fb42f66e32fb3a5cc460438aedca
Pavel Prokopenya <prokopenya.personal@gmail.com>
2024-03-05 19:02:40 +0300

    feat: add mock data files to avoid unnecessary api calls

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -2,14 +2,31 @@ import axios from "axios";
 import { HttpResponse, http } from "msw";
 
 export const handlers = [
-  // By calling "http.get()" we're instructing MSW
-  // to capture all outgoing "GET /posts" requests
-  // and execute the given response resolver when they
-  // happen.
-  http.get("https://pokeapi.co/api/v2/pokemon/pikachu/", (req, res, ctx) => {
-    // Response resolver allows you to react to captured requests,
-    // respond with mock responses or passthrough requests entirely.
-    // For now, let's just print a message to the console.
-    console.log('Captured a "GET" request', req, res, ctx);
+
+  http.get("https://api.currencyapi.com/v3/latest?apikey=cur_live_McVPn095O6nn3dK3OZmnrHf7puqxlPHC7YdDhrBY&currencies=EUR%2CUSD%2CCAD", (req) => {
+    console.log('Captured a "GET" request', req.request.url);
+    return HttpResponse.json({
+      "meta": {
+        "last_updated_at": "2024-03-03T23:59:59Z"
+      },
+      "data": {
+        "ADA": {
+          "code": "ADA",
+          "value": 1.3722768101
+        },
+        "AED": {
+          "code": "AED",
+          "value": 3.6718706476
+        },
+        "AFN": {
+          "code": "AFN",
+          "value": 73.3559138457
+        },
+        "ALL": {
+          "code": "ALL",
+          "value": 95.7827228782
+        },
+      }
+    });
   }),
 ];
