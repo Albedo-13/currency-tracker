9d2957636f92f11fd4f528f8a22f419955132f62
PavelProkopenya <prokopenya.personal@gmail.com>
2024-03-12 11:21:31 +0300

    feat: contacts section

---
+++ src/components/Contacts/Contacts.tsx
@@ -0,0 +1,34 @@
+import { Component } from "react";
+import "./contacts.scss";
+
+export default class Contacts extends Component {
+  render() {
+    return (
+      <section className="contacts">
+        <div className="container">
+          <h2 className="contacts-title gradient-text">Contacts</h2>
+          <address className="contacts-wrapper">
+            <div className="contacts-wrapper-item">
+              <p className="contacts-subtitle">Phone number</p>
+              <a className="contacts-info" href="tel:80172047194">
+                8 017 204-71-94
+              </a>
+            </div>
+
+            <div className="contacts-wrapper-item">
+              <p className="contacts-subtitle">Address</p>
+              <p className="contacts-info">10 Olshevsky Street, Minsk, Minsk region 220073</p>
+            </div>
+
+            <div className="contacts-wrapper-item">
+              <p className="contacts-subtitle">EMail</p>
+              <a className="contacts-info" href="mailto:prokopenya.work@gmail.com">
+                prokopenya.work@gmail.com
+              </a>
+            </div>
+          </address>
+        </div>
+      </section>
+    );
+  }
+}
--- src/pages/ContactPage.tsx
+++ src/pages/ContactPage.tsx
@@ -2,6 +2,7 @@ import Header from "../components/Header/Header";
 import Welcome from "../components/Welcome/Welcome";
 import UpdateStatus from "../components/UpdateStatus/UpdateStatus";
 import Footer from "../components/Footer/Footer";
+import Contacts from "../components/Contacts/Contacts";
 
 export default function MainPage() {
   return (
@@ -9,6 +10,7 @@ export default function MainPage() {
       <Header />
       <Welcome />
       <UpdateStatus />
+      <Contacts />
       <Footer />
     </>
   );
