65f83547af55119b358c10fa4c691186eb93e244
PavelProkopenya <prokopenya.personal@gmail.com>
2024-04-12 14:25:51 +0300

    feat: contact information to env file

--- src/components/Contacts/Contacts.tsx
+++ src/components/Contacts/Contacts.tsx
@@ -10,19 +10,19 @@ export default class Contacts extends Component {
           <address className="contacts-wrapper">
             <div className="contacts-wrapper-item">
               <p className="contacts-subtitle">Phone number</p>
-              <a className="contacts-info" href="tel:80172047194">
-                8 017 204-71-94
+              <a className="contacts-info" href={`tel:${import.meta.env.VITE_CONTACT_PHONE}`}>
+                {import.meta.env.VITE_CONTACT_PHONE}
               </a>
             </div>
             <div className="contacts-wrapper-item">
               <p className="contacts-subtitle">Address</p>
-              <p className="contacts-info">10 Olshevsky Street, Minsk, Minsk region 220073</p>
+              <p className="contacts-info">{import.meta.env.VITE_CONTACT_ADDRESS}</p>
             </div>
 
             <div className="contacts-wrapper-item">
               <p className="contacts-subtitle">EMail</p>
-              <a className="contacts-info" href="mailto:prokopenya.work@gmail.com">
-                prokopenya.work@gmail.com
+              <a className="contacts-info" href={`mailto:${import.meta.env.VITE_CONTACT_EMAIL}`}>
+                {import.meta.env.VITE_CONTACT_EMAIL}
               </a>
             </div>
           </address>
